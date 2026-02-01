9438317c23cd52538c842b82d8de3a956e42a453
kauzoi112 <yilmazardaa05@gmail.com>
2026-02-01 06:56:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,351 @@
 const chapters = {
     1: {
-        title: "Bölüm 1 – Başlangıç",
-        content: `Gece karanlıktı. Rüzgâr sessizce esiyordu.
-        Kimse fark etmeden kader değişmişti...`
+        title: "Bölüm 1 – Yaşamın Sırrı",
+        content: Bölüm 1. Yaşamın Sırrı. 
+
+“Dede nasıl senin gibi güçlü bir Shukumei Savaşçısı olabilirim?” Heyecanlı, parıldayan gözlerle dedesine doğru baktı, Zhao Tian. 
+
+Zhao Longyuan sakalını okşadı, kılıcını kınına koydu. Kılıç altın renginde, ejderha motifleri ile parlıyordu. Kılıcını beline koyup torununa döndü. 
+
+“Benim gibi olmak mı istiyorsun? Bu hem emeklerimin karşılığı hem de Si Ming tarafından bana bahşedilen bir şey.” 
+
+“Si Ming bana da güzel bir güç vermiştir değil mi dede?” 
+
+“Eminim ki vermiştir, eminim ki...” 
+
+... 
+
+Zhao Tian uçurumdan harika Shukumei kentine bakıyordu. Sağdan esen rüzgar Zhoa Tian’nın cübbesini sağdan sola, bayrak gibi dalgalandırıyordu. Kendisi artık bir yetişkin olmuştu. Beyaz tenine beyaz cübbesi çok güzel yakışıyordu. Tipinden asil olduğu hemen anlaşılıyordu. 
+
+Zhao Tian köylülerin arasında dolaşırken hayran dolu gözler üstünde eksik olmuyordu. Arkasında en kudretli klanlardan Zhao klanının mührü vardı. 
+
+“Torunum buralarda ne yapıyor?” Torununun omzuna sağ elini atmıştı, sakalını okşuyordu Zhao Longyuan. 
+
+“Seni arıyordum dede. Bugün büyük bir gün.” 
+
+“Haklısın. Aklımdan çıkmış. Bakalım klanımızın son üyesi nasıl bir Shukumei Buki’ye sahip olacak.” 
+
+“İçimde kötü bir his var dede. Sanki klanın yüz karası olacağım gibi.” Endişeli şekilde tahtadan yapılmış tabureye oturdu. 
+
+“İçini sıkma. Si Ming klanımızı çok seviyor, eminim ki sana güzel bir şey verecektir.” 
+
+“Si Ming nasıl görünüyor peki dede?” 
+
+“Görüntüsünü daha önce Han Yu dışında kimse görmemiş.” 
+
+“Han Yu kim?” 
+
+“Han Yu her şeyi içine çeken Shukumei Buki’ye sahipti. Si Ming’in kurduğu sisteme karşı çıkarak Tai Xu’ya ulaşmayı başaran ilk insan oldu.” 
+
+“Tai Xu nedir ve nasıl çıkılıyor?” 
+
+“Tai Xu, Si Ming’in bulunduğu yer. Orada evreni, Shukumei Buki’leri, kaderi kontrol ediyor, değiştiriyor.” Nasıl gidiliyor sorusuna cevap vermemek için unutmuş gibi yapıp arkasına döndü. 
+
+“Dede nasıl çıkıldığını söylemeyi unuttun.” 
+
+“Bunu bilmene gerek yok. Konuşmamız bile kudretli Si Ming’i sinirlendirebilir. Eğer sinirlenirse klanımızdan sadece kötü Shukumeyi Buki’yi alan kişi olabilirsin.” 
+
+Zhao Tian dedesinin dediklerini duyduktan sonra irkildi. Kendi kendine düşünüyordu, gerçekten Si Ming’i sinirlendirdiğini ve bugünün çok kötü geçeceği korkusuna kapılmıştı. Köylüler gelip geçerken dedesi çoktan gitmişti. 
+
+... 
+
+Zhao Tian heyecandan törenin olacağı yerde uyuyakalmıştı. Heyecanlı ve gergin olan Zhao Longyuan torununu uyandırmak için kılıcın kabzasıyla kafasına hafifçe vuruyordu. 
+
+“Uyanman lazım Zhao Tian, senin için önemli olan bu günde bu kadar uykucu olman klanımızı zedeler.” 
+
+“Peki dede kalkıyorum, vurmayı bırak lütfen.” Gözlerini baş parmaklarıyla ovuşturuyordu, Zhao Tian 
+
+Si Ming’in tasviri heykelinin önünde kalabalık bir grup toplandı. Grubun içerisinde çoğunlukla yaşlı, güçlü Shukumei savaşçılar yer alıyordu. Hepsi kendi arasından sohbet edip yan gözle Zhao Tian’a bakıyordu. 
+
+“Klanının ilk başarısız deneyi bu olacak sanırım, baksana yeni kalkıyor hala.” 
+
+“Katılıyorum. Dedesi şuan ne haldedir...” 
+
+Yaşlılar sohbet ederken siyah bulutlar Heykele gelen güneş ışığını kapatmıştı. Tasviri heykelin elindeki Mızrak artık sarı değil siyah parlıyordu.  
+
+“Bu olamaz! Bin yıl sonra mızrak neden siyah parlıyor. Yoksa... Düşündüğüm şey mi?” Hızlı adımlarla heykele doğru koşuyordu, Zhao Longyuan 
+
+“Bu, siyah parlaması 1000 yıl önce Han Yu’nun başına gelmişti. Büyüklerimize göre siyah parlaması uğursuzluk anlamına geliyor.” Heykele bakıp bunları sayıklamıştı bir yaşlı. 
+
+Zhao Tian, dedesinin arkasından hızlıca heykele doğru koşuyordu.  
+
+“Zhao Tian, tam şuan heykele dokunman lazım.”  
+
+Shukumei Buki’ye sahip olmak için günü gelmiş insanların heykele dokunması gerekiyordu. 
+
+“Dede ben dokunmak istemiyorum. Yaşlıların söyledikleri beni korkutuyor. Ben Shukumei Buki istemiyorum.” Göz yaşları yavaşça keskin gözlerinden damlıyordu, Zhao Tian’ın 
+
+“Dokunmak zorundasın. Si Ming kaderine bunu vermiş, kabul etmemen senin daha çok zararına olur. Yaşlıların dediklerini umursama. Tam şu an dokunman lazım!” 
+
+Zhao Tian dokunup dokunmamak arasında kararsız kalmıştı ama Si Ming’i daha fazla sinirlendirmekten daha çok korktu.  
+
+Korkusu en sonunda heykele dokunmasına vesile oldu.  
+
+Heykele dokunduğu anda siyah enerji yukarı doğru çıktı, o kadar parlaktı ki siyah olmasına rağmen herkes gözünü kapatmak zorunda kaldı.  
+
+Siyah bulutları deldi. Güneş tekrar görülüyordu. Bunlar olurken siyah enerji tamamen gitmiş yerine sarının en güzel hali gelmişti. 
+
+“Neler oluyor? Az önce siyah olan mızrak nasıl sarı olabiliyor?” İçinden geçirmişti Zhao Longyuan 
+
+Yaşlıların konuşma sesi kesilmişti, hepsi pür dikkat heykele bakıyordu. 
+
+Zhao Tian’ın ayakları yerden biraz kesilmişti. 
+
+Beyaz cübbesi havalanmaya başlamıştı, uzun saçları ise yukarı doğru dalgalanıyordu. 
+
+Elinde güzel motifli, uzun ve çok keskin bir mızrak belirdi. 
+
+Yaşlılar ve Zhao Longyuan dikkatle, şaşkınlıkla Zhao Tian’ı izliyordu. 
+
+Beyaz cübbesi yavaşça altın sarısı ve siyah rengine bürünüyordu. Elindeki mızraktan ışınlar saçılıyordu. 
+
+Tüm bulut siyahlar dağılmıştı, hava normale dönmüştü. Zhao Lungyuan hemen torununun yanına gitti. 
+
+“Dede gücüm bir lanet mi?” 
+
+“Bilmiyorum Zhao Tian daha önce böyle bir şeyi ne gördüm ne duydum.” 
+
+Bir yaşlı toplumun arasından sıyrılıp heykelin önüne kadar yavaşça geldi. 
+
+Öne çıkan ihtiyarın adı Xuanyuan Taishan. Kendisi ülkede ki Zhao Longyuan’dan sonra en güçlü kişi olarak kabul ediliyordu. 
+
+“Mızrağını sağ elinle tut, içinde ki gücü hisset ardından serbest bırak.” Yorgun gözleriyle Zhao Tian’a bakıp bunları demişti Xuanyuan Taishan. 
+
+“Ben korkuyorum. Ya elimden kontrol edemeyeceğim bir şey çıkarsa?” 
+
+“Merak etme dedenle biz buradayız.” 
+
+Zhao Tian, Xuanyuan Taishan’ın dediklerini harfi harfine yaptı. Hiçbir sonuç alınamamıştı. 
+
+“Gücü hissettiğin emin misin Zhao Tian?” 
+
+“Evet eminim, hissediyorum ama dışarı salamıyorum.” 
+
+“Az dinlensen güzel olur. Aceleye getirmemize gerek yok. Zira bu tören daha önce böyle olmamıştı.” Sakalını okşayıp torununa ciddi gözlerle bakıp bunları söylemişti Zhao Longyuan. 
+
+... 
+
+Törenden sonra Zhao Tian odasında dinleniyordu. 
+
+Gecenin geç saati olmasına rağmen odasının ışığı hala yanıyordu. 
+
+Zhao Tian gücünün ne olduğunu dikkatli şekilde düşünürken dışardan gelen bir ses düşüncesini yarıda kesmişti. 
+
+Dışarı çıkıp baktığında kısa boylu güzel bir kız evin bahçesinde geziyordu. 
+
+“Bugün törenin nasıl geçti Tian?” 
+
+Lin Xueyan, Tian’ı merak ettiği için evinin bahçesine kadar gelmiş. 
+
+Kar gibi beyaz güzel suratından dolayı Xueyan ismini almıştı. 
+
+“Yaşlılar ve dedemi ilk kez böyle telaşlı görüyorum daha önce böyle görmemiştim onları. O yüzden içimde kötü bir his var.” 
+
+“Eminim ki düşündüğün kadar kötü bir şey olmamıştır. Bugün olan olayları köylülerden duydum. Sıra dışı bir olay lakin Si Ming’in klanınıza kötü davranacağını düşünmüyorum.” 
+
+Lin Xueyan’ın dediklerinden sonra Zhao Tian’ın kalbine biraz daha su serpilmişti. 
+
+“Ne düşünüyorsun peki sence gücüm çok güçlü bir şey midir?” Şakacı bir gülümseme ile bu soruyu sormuştu Zhao Tian. 
+
+“Güçlü bir gücünün olduğuna hiç bir şüphe yok.” Elleri ile ağzını kapatıp hafif gülümsemişti, Lin Xueyan. 
+
+“Bu saatte yalnız dolaşman tehlikeli, seni ben bırakabilirim.” 
+
+“Sende beni çok güçsüz sandın herhalde.” Gülümsemesini biraz daha devam ettirip elindeki yelpaze şeklinde ki Shukumei Buki’sini göstermişti. 
+
+Lin Xueyan’ın gücü cazibesini kullanıp insanları bir nebze manipüle etmesine yarıyor.  
+
+“Doğru, affet beni.” Ellerini arkada kavuşturmuş bir şekilde bunları söyledi Zhao Tian. 
+
+“Affetme mi istiyorsan gücünü öğrendikten sonra bana Mijiu ısmarla anca o zaman affederim.” 
+
+Bunları dedikten sonra arkasını dönüp yavaş, zarif adımlarla gitmeye başlamıştı. 
+
+Zhao Tian aşırı duygusal gözleriyle Lin Xueyan’ın yürümesine bakıyordu. 
+
+... 
+
+Klan tapınağının bahçesinde ki sessizlikte gücünü uyandırmaya çalışıyordu, Zhao Tian. 
+
+Klanının adını kötüye çıkarmamak için elinden geleni yapmaya çalışıyordu en güzel şekilde. 
+
+107. Denemeden sonra aklına Lin Xueyan ile konuştuğu sohbet geldi. 
+
+Tekrar denedikten sonra nihayet 108. Denemede gücünü uyandırmıştı. 
+
+Tüm şehri harika bir altın sarısı kaplamıştı. İnsanlar yine fazla parlaklığa gözlerini kısıyordu. 
+
+Zhao Longyuan salondan koşarak bahçeye doğru gelmişti. 
+
+“Zhao Tian inanamıyorum! Arkandaki bu figür daha önce karşılaşılmamış bir şey.” 
+
+Zhao Tian dedesinin bu dediklerinden sonra hızlı bir şekilde başını arkaya çevirdi. 
+
+Not: 
+
+Shukumei Buki tanrı tarafından insanlara verilen mistik, metafiziksel bir güç. İnsanların kendi kaderlerine göre farklı silahlar farklı güçler uyanabiliyor. 
+
+Si Ming evrenin tanrısıdır. Görüşünü hakkında hiç bir bilgi yoktur. Gücü ise dahada gizlidir. 
     },
     2: {
-        title: "Bölüm 2 – Karşılaşma",
-        content: `O anda kapı açıldı.
-        Hayatımı sonsuza kadar değiştirecek kişi karşımdadaydı.`
+        title: "Bölüm 2 – Kader Uyanışı",
+        content: `Zhao Tian arkasını döndüğünde kocaman bir Buda heykeli vardı. Şaşkın gözleriyle heykeli yukardan aşağıya seziyordu.  
+
+Heykel saf altın sarısından oluşmakla kalmıyordu, bazı yerlerini de saf siyahlık sarıyordu.  
+
+Heykel bir kaç saniye gözüktükten sonra gitmişti. 
+
+“Zhao Tian! Uyandırdığın gücün farkında mısın?” 
+
+“Dede bu güçte ne? Lanet olması gerekmiyor muydu? Neden Shukumei Buki’m Buda? 
+
+“Bilmiyorum Zhao Tian. İlk defa  böyle bir durumla karşı karşıyayız.” Sakallarını okşuyordu yavaşça Zhao Longyuan. 
+
+Hafifçe yağmur yağmaya başlamıştı. Zhao Tian’ın giydiği zümrüt yeşili cübbesine tek tek damlalar damlıyordu.  
+
+Yağan yağmur damlaları yere düşmeden yok oluyordu.  
+
+Zhao Longyuan ne olduğunu anlamaya çalışmaya kalmadan yerde siyah parçacıklar oluşuyordu. 
+
+“Zhao Tian gücünü kullanmayı hemen bırak!” 
+
+“Dede ben bir şey yapmıyorum!” dedikten sonra yerde siyah garip noktaların varlığını görmüştü. 
+
+Zhao Tian’ın gücünü kontrol edemediğini gören Zhao Longyuan kılıcını kabzasından çıkartmıştı. 
+
+Harika bir beyaz ışık yayılmıştı, arkasında kocaman bir Ejderha belirmişti. 
+
+“Bunu yapmak istemiyorum ama zorundayım sevgili torunum.” 
+
+İleri doğru çok hızlı bir şekilde atıldı. Kat ettiği her yol güzel, huzur veren beyaz ışık ile aydınlanıyordu. 
+
+Geçtiği siyah noktalar ise yok oluyordu. 
+
+Ani bir atılma ile kılıcını sol tarafdan hızlı bir şekilde sağ tarafa hareket ettirdi. Atak yaptığı yer Zhao Tian’ın kalbine yakın bir yerdeydi. 
+
+Zhao Longyuan kılıcını Zhao Tian’ın üstünde bekletiyordu ardından bir kaç kelime sayıklamaya başlamıştı. 
+
+“Ey kudretli Si Ming! Senden torunuma göz kulak olmanı istiyorum. Benim gücümle birlikte torunum Han Yu’nun lanetli gücünü bastırsın.”  
+
+Bu yaptığı büyüden sonra Zhao Tian’ın kalbine yakın bir yerde beyaz bir ışık belirmişti.  
+
+Şaşkın gözlerle kendi vücuduna bakıyordu Zhao Tian. 
+
+“Dede bu beyaz ışıkta ne oluyor? Beni öldüreceksin diye çok korktum.” 
+
+“Endişelenmeni gerektirecek durum yok, Han Yu’nun gücünü kontrol altına alabilmek için kendi büyümü vücüduna işledim.” 
+
+“Han Yu’mu? Han Yu’nun gücü benim içimde mi?” 
+
+“Tahminlerime göre şuanlık durum onu gösteriyor Zhao Tian.” endişeli şekilde sakallarıın okşuyordu Zhao Longyuan. 
+
+Dedesinin açıklamalarından sonra kendisine Si Ming tarafından kötü kaderin tayin edildiğini düşünüyordu. 
+
+“Yapman gereken tek şey gidip dinlenmen. Gücünü yeni uyandırdın yorulmuşsundur.” 
+
+Zhao Tian cevap vermeden sessizce yağan yağmurun altından odasına doğru gidiyordu. 
+
+... 
+
+Uykusundan kalkan Zhao Tian yorgun gözlerle çok sevdiği mekana doğru yol alıyordu. 
+
+Kapıdan çıktıktan sonra onu heyecanlı, sabırsız şekilde bekleyen Lin Xueyan karşılamıştı. 
+
+“Yeni gücünü uyandırdıktan sonra hiç haber etme zahmetinde bulunmuyorsun Zhao Tian. Bana verdiğin Mijiu sözünü boşver, haber etme sözünü bile tutmadın.” 
+
+“Özür dilerim. Gücümü uyandırdıktan sonra hiç beklenmedim şeyler oldu, yorgunluğumdan sonrada dedem dinlenmemi söyledi.” bunları dedikten sonra kalbinin yakın bir yerine dedesinin bıraktığı mührü göstermişti. 
+
+“Ciddiye almana gerek yok şakalaşıyorum seninle. Bu halini görmek çok keyif verici olabiliyor. Ayrıca dün olan olaylardan da haberim var.” sessiz ve zarif bir şekilde hafifce gülümsedi. 
+
+“Dedem mi haber verdi?” 
+
+“Evet.” 
+
+İki taraf gözleri yaşarana kadar gülüyordu.  
+
+Zhao Tian, Lin Xueyan’ın gözlerine derin derin bakıyordu, adeta büyüsüne kapılmış gibiydi. 
+
+“Benim üstümde gücünü kullanmıyorsun değil mi?” 
+
+“Hayır tabii ki de. İlginç bir soru oldu, neden sordun?” 
+
+“Gözüme çok güzel geliyorsun, gözlerin bambaşka geliyor.” 
+
+Bu cümleden sonra Zhao Tian’ın bembeyaz yüzü, utançlıktan kıpkırmızı kesilmişti. 
+
+Lin Xueyan ise sağ eli ile ağzını kapatmış alttan alttan cilveli şekilde gülümsüyordu. 
+
+“Bu saatte nereye gidiyorsun bu arada?” 
+
+“Wan Mu Yuan parkına gideceğim.” 
+
+“Orayı çok seviyorsun, ne zaman görsem, birilerine sorsam o parkta takılıyorsun.” 
+
+“Evet. Kendimi huzurlu hissettiğim nadir mekanlardan biridir Wan Mu Yuan. İstersen bana eşlik edebilirsin.” 
+
+“Davetin için teşekkür ederim, teklifini memnuniyetle kabul ediyorum.” 
+
+Gün ışığı altında, hafif rüzgarlı havada iki genç Wan Mu Yuan’a doğru gidiyordu. 
+
+... 
+
+Duvarları siyah, bin adet Si Ming heykeli bulunan bir tapınağın içinde bir grup yaşlı insan bir meseleyi tartışıyordu. 
+
+“Zhao klanı her ne kadar önemli olsa da, Zhao Tian’ın bu ülkede bulundurulması söz konusu dahi olamaz.” 
+
+“Katılıyorum... Zira Han Yu gibi bir hainin, tanrıya kafa kaldıran bir insanla aynı gücü paylaşıyor. Hatta aynı güç değil direkt Han Yu’nun kaderini taşıyor.” 
+
+“Ne kadar tehlikeli olabildiğini bu açıklama ile görebiliyoruz. Bir oylama ile gönderip göndermeyeceğimize karar verelim.” 
+
+İçeri sakin yavaş adımlarla Zhao Longyuan yürüyordu. Boş koridorda, Zhao Longyuan’nın kılıcınının kabzasının yere değme sesleri yankılanıyordu. 
+
+Yaşlılardan oluşan meclis anında suskunluğa kapılmıştı. 
+
+“Nihayet beklenen kişi geldi.” 
+
+“Ülkede böyle kararlar alınabilmesi için, torunumu bu denli tehlikeli görmeniz için beni yok saymanız gerek. Zira ülkede hiç güçlü Shukumeyi savaşçısı kalmamış gibi davranıyorsunuz sevgili meclis.” 
+
+“Güçlü olsan bile önüne duyguların geçebilir Zhao Longyuan. Büyük savaşçı olduğunu kimse ret etmiyor.” 
+
+“Gayet mantıklı düşünüyorsunuz bu konuda, lakin Si Ming’e sadakatim için her şeyi göze alabilirim.” 
+
+“Biz yine de garantiye almayı yeğliyoruz sevgili Zhao Longyuan.” 
+
+“Bu dünya da tehlikenin önüne geçemezsiniz. Bu ülkenin dışında kaç tane tehlikeli güç, varlık, gruplar var.” 
+
+“Bunları inkar eden yok, olay duygunun önüne geçmesi.” 
+
+“Anlıyorum. Haksız sayılmazsınız. O halde oylama yapılabilir.” 
+
+“Sözlerimi yanlış anlama lakin oylama yapılma izni vermeni bekleyen yoktu. Ek olarak oylamamızın da yarıda kesilmesine sebebiyet oldun.” 
+
+Bu sözlerden sonra Zhao Longyuan gururuna yediremeyip kılıcını kabzasından çıkarttı. Etrafa yine beyaz bir ışık yayılmıştı. 
+
+Büyük bir sandalyeden ayağa kalkan Xuanyuan Taishan, Zhao Longyuan’a doğru yürüyüp ellerine çift silah kuşanmıştı. 
+
+Silahların birisinden yeşil bir parlaklık yükselirken, diğerinden ise turuncu bir parlaklık yükseliyordu. 
+
+“Bu mücadelemizi burada yapmayacağımızı ikimizde biliyoruz. Senden durmanı rica edeceğim Zhao Longyuan.” 
+
+“Dürüst olmam gerekirse seni pekte dinleyesim yok Xuanyuan Taishan.” 
+
+“Peki madem, öyleyse bu işi burada bitirmekten başka çaremiz yok.” 
+
+İkiside ileriye doğru çok hızlı şekilde atılmıştı. Zhao Longyuan, Xuanyuan Taishan’a nazaran biraz daha hızlıydı. İkiside birbirlerine tam isabetli saldırı yapacakları esnada gökten ışık hızında bir anda inen varlık belirdi. 
+
+Varlığın bir görünüşü yoktu, her yeri bembeyazdı. 
+
+“İkinize durmanızı emrediyorum!” 
+
+Zhao Longyuan ve Xuanyuan Taishan beyaz varlığın dediğini anında dinledi, hiç karşı çıkmayı düşünmediler bile. 
+
+Longyuan, Taishan’da dahil tüm meclis şaşkınlık içerisindeydi. Meclisteki herkesin dizleri titriyor, ayakta kalmakta zorlanıyorlardı. 
+
+“Sizi bu olayla, buraya kadar yorduğumuz için özür dileriz kudretli Si Ming” bu sözleri söyledikten hemen sonra tüm Longyuan ve tüm meclis dizlerinin üstüne çökmüşlerdi. 
+
+“Size bu güçleri birbirinizle savaşın diye vermedim. Oylamaya da gerek yok. Zhao Tian bu ülkeden sürülecek.” dedikten sonra Si Ming’in yanılsaması anında yok olmuştu. 
+
+Tüm meclis hiç bir söz etmeden hemen dağılmıştı.`
     },
     3: {
         title: "Bölüm 3 – Seçim",
@@ -20,3 +358,4 @@ function loadChapter(num) {
     document.getElementById("chapter-title").innerText = chapters[num].title;
     document.getElementById("chapter-content").innerText = chapters[num].content;
 }
+
